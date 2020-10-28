1b4ef5515d7db6f85abd6ec46e83bf21512d7e73
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2020-10-27 20:13:16 -0400

    Add functionality for "Show Entries" subsection of "Templates and Views" - Fails even more tests than before, but we're getting there

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template
 
 
 # configuration
@@ -44,8 +44,12 @@ def close_db(error):
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    """Searches the database for entries, then displays them."""
+    db = get_db()
+    cur = db.execute("select * from entries order by id desc")
+    entries = cur.fetchall()
+    return render_template("index.html", entries=entries)
 
 
 if __name__ == "__main__":
