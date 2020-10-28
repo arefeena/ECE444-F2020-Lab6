53bb642a9a18c23bcc54e4ae578a058282ff280c
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2020-10-27 20:18:54 -0400

    Complete "Templates and Views" section of lab - Add `/add` endpoint, whose absence caused the `werkzeug.routing.BuildError` - All 5 tests should now pass

--- project/app.py
+++ project/app.py
@@ -1,5 +1,15 @@
 import sqlite3
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for
+from flask import (
+    Flask,
+    g,
+    render_template,
+    request,
+    session,
+    flash,
+    redirect,
+    url_for,
+    abort,
+)
 
 
 # configuration
@@ -69,6 +79,21 @@ def login():
             flash("You were logged in")
             return redirect(url_for("index"))
     return render_template("login.html", error=error)
+
+
+@app.route("/add", methods=["POST"])
+def add_entry():
+    """Add new post to database."""
+    if not session.get("logged_in"):
+        abort(401)
+    db = get_db()
+    db.execute(
+        "insert into entries (title, text) values (?, ?)",
+        [request.form["title"], request.form["text"]],
+    )
+    db.commit()
+    flash("New entry was successfully posted")
+    return redirect(url_for("index"))
 
 
 @app.route("/logout")
