1c142fb5680291faa2f111330f3049d895f7974e
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2020-10-29 13:44:29 -0400

    Fix import ordering error

--- project/app.py
+++ project/app.py
@@ -13,7 +13,6 @@ from flask import (
     jsonify,
 )
 from flask_sqlalchemy import SQLAlchemy
-from project import models
 
 basedir = Path(__file__).resolve().parent
 
@@ -33,6 +32,7 @@ app.config.from_object(__name__)
 # init sqlalchemy
 db = SQLAlchemy(app)
 
+from project import models
 
 def login_required(f):
     @wraps(f)
